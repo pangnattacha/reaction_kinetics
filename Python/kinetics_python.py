729f90198bc398c2f73ca47527ddc86f0a494dae
Nattacha Paksung <pang.nattacha@gmail.com>
2019-06-11 15:38:56 +0200

    Update kinetics_python.py

--- Python/kinetics_python.py
+++ Python/kinetics_python.py
@@ -1,7 +1,4 @@
 # import libraries
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy import optimize
 
@@ -35,43 +32,3 @@ class Optimize_k():
     # define function to optimize k values
     def optim(self,k_guess):
         return optimize.leastsq(self.errfun,k_guess)
-
-# define function to load data, optimize k and plot the result
-def fun1(temp):
-    # Read excel file
-    table = pd.read_excel('data_dummy.xlsx')
-    idx = table['Temp'] == temp
-    t = table[idx].time
-    comp = ['A','B','C','D']
-    y = table[idx][comp]
-    
-    # Assign Optimize_k
-    ok = Optimize_k(y,t)
-  
-    # Initialize k values
-    k1,k2,k3 = 0.,0.,0.
-    # Bundle parameters for ODE solver
-    k_guess = [k1,k2,k3]
-  
-    # optimize k values
-    k_optim,kvg = ok.optim(k_guess)
-  
-    # predict values from k_optim
-    t_span = np.arange(0,t.iloc[-1]+0.1,0.1)
-    x = ok.model(t_span,k_optim)
-    
-    # plot data comparing to the prediction
-    plt.plot(t_span,x)
-    plt.scatter(t,y['A'])
-    plt.scatter(t,y['B'])
-    plt.scatter(t,y['C'])
-    plt.scatter(t,y['D'])
-    plt.legend(['predicted A','predicted B','predicted C','predicted D',
-                'A','B','C','D'])
-    plt.xlabel('time [min]')
-    plt.ylabel(r'concentration [mol.L$^{-1}$]')
-    plt.xlim(0,60)
-    plt.ylim(0,1)
-    plt.plot()
-    
-    return k_optim
